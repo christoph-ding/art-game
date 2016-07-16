07b257ff881ba60af3914a9e30afca1a0870ab73
Pokegear <christoph.ding@gmail.com>
2016-07-16 15:04:31 -0400

    Add routing to server.js.  Now the server serves the static files

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
-var http = require("http");
-var url = require("url")
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   // routing
   var reqInfo = url.parse(req.url)
-  var pathname = reqInfo.pathname
-  console.log("Client:  " +  req.connection.remoteAddress + "path:  " + pathname);
+  var pathname = reqInfo.pathname;
+  console.log('request starting for path:  ' + pathname);
 
-  resMsg = "heya";
   // static files
-  if (pathname === "/") {
-    resMsg = "...serving index.html";
+  if (pathname === '/') {
+    fs.readFile(__dirname + '/../client/index.html', function(err, data) {
+      if (err) {
+        console.log(err);
+      };
+      res.writeHead(200, {'Content-type': 'text/html'});
+      res.end(data);
+    })
   }
 
-  //
-  res.writeHead(200);
-  res.end(resMsg);
+  else if (pathname === '/style.css') {
+    fs.readFile(__dirname + '/../client/style.css', function(err, data) {
+      console.log('hello: ' + __dirname);
+      res.end(data);
+    });
+  }
+
+  else {
+    res.writeHead(404);
+    res.end('wtf, could not find: ' + pathname);
+  }
 })
 
 server.listen(8000);
