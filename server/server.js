781385c8bfbb3577d136e1fe30c06db571e54bb2
Pokegear <christoph.ding@gmail.com>
2016-07-16 13:43:15 -0400

    Add url to node to parse the request url and provide routing

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
-var http = require("http")
+var http = require("http");
+var url = require("url")
 
 var server = http.createServer(function(req, res) {
+  // routing
+  var reqInfo = url.parse(req.url)
+  var pathname = reqInfo.pathname
+  console.log("Client:  " +  req.connection.remoteAddress + "path:  " + pathname);
+
+  resMsg = "heya";
+  // static files
+  if (pathname === "/") {
+    resMsg = "...serving index.html";
+  }
+
+  //
   res.writeHead(200);
-  res.end("heya");
+  res.end(resMsg);
 })
 
 server.listen(8000);
