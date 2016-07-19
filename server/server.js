89357f83d3580410bf2c7918066088fc2060972b
Pokegear <christoph.ding@gmail.com>
2016-07-19 19:50:57 -0400

    Refactor to use Express.  Server serves static files.  Basic ui-routing in client

--- server/server.js
+++ server/server.js
@@ -1,66 +1,13 @@
-var http = require('http');
-var url = require('url');
+// dependencies
+var express = require('express');
 var path = require('path');
-var fs = require('fs');
 
-// Variables for pathing
-var extensions = {
-  '.html': "text/html",
-  '.css': "text/css",
-  '.js': "text/js"
-};
+// server and connection
+var app = express();
+var port = 8000;
+app.listen(port);
 
-var directories = {
-  client: '/../client/',
-  bower_components: '/../client/bower_components/',
-  pages: '/../client/pages/'
-}
+console.log('heyo!');
 
-var files = {
-    '/': directories.client + 'index.html',
-    '/style.css': directories.client + 'style.css',
-    '/bower_components/angular/angular.js': directories.bower_components + 'angular/angular.js',
-    '/bower_components/angular-ui-router/release/angular-ui-router.js': directories.bower_components + 'angular-ui-router/release/angular-ui-router.js',
-    '/app.js': directories.client + 'app.js',
-    // 'Pages'
-    '/cover.html': directories.pages + 'cover.html',
-    '/about.html': directories.pages + 'about.html'
-};
-
-var server = http.createServer(function(req, res) {
-  // routing
-  var reqPath = url.parse(req.url).pathname;
-  var filePath = path.join(__dirname + files[reqPath]);
-  console.log('requested file: ' + filePath + '       ' + reqPath);
-
-  fs.exists(filePath, function(exists) {
-    ext = path.extname(filePath);
-    if (exists) {
-      // why do I need this fucking exception for .css files?
-      if (ext === '.css') {
-        fs.readFile(filePath, function(err, data){
-          if (err) console.log(err)
-          else {
-            res.writeHead(200, {'Content-Type': 'text/css'});
-            res.write(data);
-            res.end();
-          }
-        })
-      } else {
-        fs.readFile(filePath, function(err, data){
-          if (err) console.log(err)
-          else {
-            res.writeHead(200, {'Content-Type': extensions[ext]});
-            res.write(data);
-            res.end();
-          }
-        })
-      }
-    } else {
-    res.writeHead(404);
-    res.end('wtf, could not find: ' + filePath);
-    }
-  })
-})
-
-server.listen(8000);
+// routing
+app.use(express.static(path.join(__dirname + '/../client')));
