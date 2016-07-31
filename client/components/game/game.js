77b7255707bae5143f95100fdcc2d5c7789873dc
Pokegear <christoph.ding@gmail.com>
2016-07-31 12:47:07 -0400

    Change card behavior: they will not flip from faceshowing to back again simply by clicking on the card. Added score

--- client/components/game/game.js
+++ client/components/game/game.js
@@ -1,6 +1,6 @@
 angular.module('game', [])
 .controller('gameController', ['gamestarter', 'cardTracker', function(gamestarter, cardTracker) {
-  this.board = gamestarter.generateBoard();
+  this.board = gamestarter.generateBoard(3,5);
   this.tracker = cardTracker.generateTracker();
 
   this.flipCard = function(card) {
