7d0bb9dc142e6f3434e38b49bf9607e094c89100
Pokegear <christoph.ding@gmail.com>
2016-07-31 13:44:25 -0400

    Refactored the cardtracker to flip cards back on a timer after checking if both flipped cards match

--- client/components/game/game.js
+++ client/components/game/game.js
@@ -1,12 +1,11 @@
 angular.module('game', [])
-.controller('gameController', ['gamestarter', 'cardTracker', function(gamestarter, cardTracker) {
+.controller('gameController', function(gamestarter, cardTracker) {
   this.board = gamestarter.generateBoard(3,5);
   this.tracker = cardTracker.generateTracker();
 
   this.flipCard = function(card) {
     card.flip();
     this.tracker.doSomething(card);
-
   }
 
-}])
+})
