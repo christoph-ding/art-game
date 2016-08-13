d3bbedb1fd3b07a4e235e7bfc3b8e37b7ca1eeb7
Pokegear <christoph.ding@gmail.com>
2016-08-13 16:39:14 -0400

    Add new class for images, so that the gallery has images objects analougous to the cards in the game, rather than simply image DOM objects.
    Now the images fetched in the gallery will be scaled to a uniform size

--- client/components/gallery/gallery.js
+++ client/components/gallery/gallery.js
@@ -1,19 +1,12 @@
 angular.module('gallery', [])
 .controller('galleryController', function(imageFetcher) {
   var gallery = this;
-  var imagesDir = '/assets/images/hiroshige/'
-  gallery.deck = [ { 'id': 1 }, { 'id': 2 }, { 'id': 3 },
-               { 'id': 4 }, { 'id': 5 }, { 'id': 6 },
-               { 'id': 7 }, { 'id': 8 }, { 'id': 9 },
-               { 'id': 10 }, { 'id': 11 }, { 'id': 12 }
-             ]
+  var imagesDir = '/assets/images/hiroshige/';
+  gallery.collection = null;
 
   imageFetcher.fetchImages()
     .then(function(files) {
-      gallery.map = imageFetcher.generateMap(files.data, gallery.deck.length / 2);
-    })
-    .then(function() {
-      imageFetcher.attachImagesToCards(gallery.deck, gallery.map, imagesDir);
-    })
-
+      console.log(files);
+      gallery.collection = files.data;
+  })
 })
