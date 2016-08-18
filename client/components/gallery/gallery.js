fbcb0b92bbd592858bf74ecc4e803575809df5f0
Pokegear <christoph.ding@gmail.com>
2016-08-18 18:49:57 -0400

    Clicking on a gallery image creates a modal that covers the screen

--- client/components/gallery/gallery.js
+++ client/components/gallery/gallery.js
@@ -7,17 +7,19 @@ angular.module('gallery', [])
 
   gallery.focusModal = { 
     msg: 'hello', 
-    url: 'http://image.flaticon.com/teams/1-freepik.jpg'
-    };
+    url: 'http://image.flaticon.com/teams/1-freepik.jpg',
+    class: 'hidden'
+  };
 
   gallery.changeFocus = function(image) {
     console.log(image.URL);
     gallery.focusModal.url = image.URL;
-  }
+    gallery.focusModal.class = 'modal'
+  };
 
   imageFetcher.fetchImages()
     .then(function(files) {
       console.log(files);
       gallery.collection = imageFetcher.generateCollectionOfImages(files.data, imagesDir);
-  })
+  });
 })
