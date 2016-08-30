bf9244dc657a2b61d8488f560a4232e08e216735
Pokegear <christoph.ding@gmail.com>
2016-08-30 14:41:07 -0400

    Added roundhandling functionality to the game

--- client/components/game/game.js
+++ client/components/game/game.js
@@ -3,15 +3,12 @@ angular.module('game', [])
 
   var game = this;
 
-  // get a deck of cards
-  game.deck = deckOfCards.getDeck(10);
-
-  console.log(game.deck);
-
-  // get a board using that deck
-
+  // generate the deck, board and roundHandler neccesary to play game
+  game.deck = deckOfCards.getDeck(20);
+  game.board = deckOfCards.generateBoard(4, 5, game.deck);
   // get a roundHandler
-    
+  game.roundHandler = roundHandler.generateRoundHandler();
+
   game.flipCard = function(card) {
     // we will not flip cards if the game is 'processing' result
     // we will not allow players to flip the same card
